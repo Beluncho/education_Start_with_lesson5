48af409a064d2e54c969e33c6a627b1acf2de70d
Beluncho <belov-bk@mail.ru>
2024-02-06 20:08:38 +0300

    add lesson 8

---
+++ test_save_hand_made.py
@@ -0,0 +1,9 @@
+from def_for_manager import *
+from victory import *
+
+# print(view_file())
+# print(view_dir())
+# print(save_dir())
+# print(save_file())
+
+victory()
--- use_function.py
+++ use_function.py
@@ -1,13 +1,29 @@
+import os
+import pickle
+import json
+from decorator_example import add_sep
+
+
 def balance_fun():
     balance = 0.0
     buy_story = []
+    BALANCE_FILE_NAME = 'balance.data'
+    BUY_FILE_NAME = 'buy_story.data'
+
+    if os.path.exists(BALANCE_FILE_NAME):
+        with open(BALANCE_FILE_NAME, 'r') as f:
+            balance = json.load(f)
+    if os.path.exists(BUY_FILE_NAME):
+        with open(BUY_FILE_NAME, 'rb') as f:
+            buy_story = pickle.load(f)
+    @add_sep
     def refill(balance):
         ref = float(input('enter sum: '))
         balance += ref
         return balance
     pass
 
-
+    @add_sep
     def yes_buy(balance):
         buy = float(input('enter buy sum:'))
 
@@ -28,15 +44,25 @@ def balance_fun():
         print('Ваш баланс:', balance)
         choice = input('Выберите пункт меню')
         if choice == '1':
-            balance = refill(balance)
+            try:
+                balance = refill(balance)
+            except ValueError:
+                print("Введите цифры")
             pass
         elif choice == '2':
-            balance = yes_buy(balance)
+            try:
+                balance = yes_buy(balance)
+            except ValueError:
+                print('error')
             pass
         elif choice == '3':
             print(buy_story)
             pass
         elif choice == '4':
+            with open(BALANCE_FILE_NAME, 'w') as f:
+                json.dump(balance, f)
+                with open(BUY_FILE_NAME, 'wb') as f:
+                    pickle.dump(buy_story, f)
             break
         else:
             print('Неверный пункт меню')
